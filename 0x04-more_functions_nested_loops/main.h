48bdc7fe28837062f04086902c15ca1dc8d33a89
Joseph Kakai <josephngalu96gmail.com>
2022-11-26 19:08:04 +0300

    added file

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <unistd.h>
+
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+int _putchar(char c);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+#endif
